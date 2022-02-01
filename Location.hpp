c6fcb815f83a842b23ca69781bc8ac0b71aaa12b
Abel Van bergen en henegouwen <avan-ber@f0r5s11.codam.nl>
2022-02-01 17:16:24 +0100

    bladi bladi blad

--- Location.hpp
+++ Location.hpp
@@ -1,25 +1,25 @@
 #ifndef LOCATION_HPP
 # define LOCATION_HPP
 
-typedef struct		s_redir
-{
-	int				status_code;
-	std::string		location;
-}					t_redir;
+# include <iostream>
+# include <vector>
+# include <set>
+# include <map>
+# include "redir.hpp"
 
 class	Location
 {
 	private:
-		std::string							_title;
+		// std::string							_title;
 		std::string							_root;
-		unsigned long						_clientMaxBodySize;
-		std::vector<std::string>			_index;
-		bool								_autoindex;
-		bool								_staticDir;
-		std::map<std::string, std::string>	_cgi;
-		std::set<std::string>				_limitExcept;
-		std::string							_uploadStore;
-		Redir								_redir;
+		// unsigned long						_clientMaxBodySize;
+		// std::vector<std::string>			_index;
+		// bool								_autoindex;
+		// bool								_staticDir;
+		// std::map<std::string, std::string>	_cgi;
+		// std::set<std::string>				_limitExcept;
+		// std::string							_uploadStore;
+		// Redir								_redir;
 
 	public:
 		Location();
@@ -30,42 +30,48 @@ class	Location
 		// setters
 		// void	setTitle();
 		void	setRoot(std::vector<std::string>& line);
-		void	setClientMaxBodySize(std::vector<std::string>& line);
-		void	setIndex(std::vector<std::string>& line);
-		void	setAutoindex(std::vector<std::string>& line);
-		void	setStaticDir(std::vector<std::string>& line);
-		void	addCgi(std::vector<std::string>& line);
-		void	setLimitExcept(std::vector<std::string>& line);
-		void	setUploadStore(std::vector<std::string>& line);
-		void	setRedir(std::vector<std::string>& line);
+		// void	setClientMaxBodySize(std::vector<std::string>& line);
+		// void	setIndex(std::vector<std::string>& line);
+		// void	setAutoindex(std::vector<std::string>& line);
+		// void	setStaticDir(std::vector<std::string>& line);
+		// void	addCgi(std::vector<std::string>& line);
+		// void	setLimitExcept(std::vector<std::string>& line);
+		// void	setUploadStore(std::vector<std::string>& line);
+		// void	setRedir(std::vector<std::string>& line);
 
 		//getters
 		const std::string&							getRoot() const;
-		const unsigned long							getClientMaxBodySize() const;
-		const std::vector<std::string>&				getIndex() const;
-		const bool									getAutoindex() const;
-		const bool									getStaticDir() const;
-		const std::map<std::string, std::string>&	getCgi() const;
-		const std::set<std::string>&				getLimitExcept() const;
-		const std::string&							getUploadStore() const;
-		const Redir&								getRedir() const;
+		// unsigned long							getClientMaxBodySize() const;
+		// std::vector<std::string>&				getIndex() const;
+		// bool									getAutoindex() const;
+		// bool									getStaticDir() const;
+		// std::map<std::string, std::string>&		getCgi() const;
+		// std::set<std::string>&					getLimitExcept() const;
+		// std::string&							getUploadStore() const;
+		// Redir&									getRedir() const;
 
-		class ArgumentIncorrect : public std::exception
-		{
-			private:
-				std::vector<std::string>	_line;
+		// class ArgumentIncorrect : public std::exception
+		// {
+		// 	private:
+		// 		std::vector<std::string>	_line;
 			
-			const char*	what(void) const throw()
-			{
-				std::string	ret;
+		// 	const char*	what(void) const throw()
+		// 	{
+		// 		std::string	ret;
 
-				ret = "Incorrect argument amount:"
-				for (size_t i = 0; i < this->_line.size(); i++)
-					ret += " " + this->_line[i];
-				return ret.c_str();
-			};
+		// 		ret = "Incorrect argument amount:";
+		// 		for (size_t i = 0; i < this->_line.size(); i++)
+		// 			ret += " " + this->_line[i];
+		// 		return ret.c_str();
+		// 	}
 
-			ArgumentIncorrect(std::vector<std::string> line) : _line(line);
-		};
+			// ArgumentIncorrect(std::vector<std::string> line) : _line(line)
+			// {
+			// 	return ;
+			// }
+		// };
+};//end Location class
+
+std::ostream&	operator<< (std::ostream& out, const Location& obj);
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,93 +1,29 @@
 
-// #include "Server.hpp"
 # include <iostream>
 # include <fstream>
 # include <vector>
-# include "utils.hpp"
+// # include "utils.hpp"
+#include "Location.hpp"
 
-// bool	serverStart(std::vector<std::string> config, size_t idx)
+// template<class T>
+// void	parseLoop(T& block, std::deque<std::string>& config)
 // {
-// 	if (config[idx] == "server {")
-// 		return true;
-// 	if (idx + 1 < config.size() && config[idx] = "server" && config[idx + 1] == "{")
-// 		return true;
-// 	return false;
+// 	while (!config.empty())
+// 	{
+// 		std::string& line = config.front();
+// 		config.pop_front();
+// 		std::vector<std::string> splitted = split(line);
+// 		fptr setfunc = block.switchcase(splitted[0]);
+// 		if (setfunc == NULL)
+// 			return ;
+// 		block.(*setfunc)(splitted);
+// 	}
 // }
 
-void	setLocation(std::vector<std::string> config, int* i)
+int main(void)
 {
-	location
-	while (*i < config.size())
-	{
-		std::vector<std::string> split;
-		split = split(config[*i]);
-		if (split.len() == 0)
-			continue ;
-		switch(split[0])\{
-			case "}":
-				return ;
-			case "root":
-				loc.setRoot();
-				break ;
-			case "client_max_body_size":
-				loc.setMaxBodySize();
-				break ;
-			case "index":
-				loc.setIndex();
-				break ;
-			case "autoindex":
-				loc.setAutoIndex();
-				break ;
-			case "static_dir":
-				loc.setStaticDir();
-				break ;
-			case "cgi":
-				loc.setCgi();
-				break ;
-			case "limit_except":
-				loc.setLimitExcept();
-				break ;
-			case "upload_store":
-				loc.setUploadStore();
-				break ;
-			case "redir":
-				loc.setUploadStore();
-				break ;
-			default:
-				trow();
-		}
-	}
+	Location loc;
+
+	std::cout << loc << std::endl;
 }
 
-void	setServer(std::vector<std::string> config, int* i)
-{
-	while (*i < )
-	{
-		if (config[*i] == "\t{")
-			break ;
-		
-	}
-}
-
-void	setServers(std::vector<std::string> config)
-{
-
-}
-
-int	main(void)
-{
-	std::vector<std::string>	file;
-	std::string					input;
-	std::ifstream				stream;
-
-	file.reserve(48);
-	stream.open("config.conf");
-	// Server	main(stream);
-	while (getline(stream, input))
-	{
-		file.push_back(input);
-	}
-	for (size_t i = 0; i < file.size(); i++)
-		std::cout << file[i] << std::endl;
-	return (0);
-}
