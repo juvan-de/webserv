361d8fdfed994ad55c4cc2341bd7fffb636f8ad3
Jules laurens Van der hoek <juvan-de@f0r6s9.codam.nl>
2022-03-21 16:39:30 +0100

    yes

--- srcs/connection/handle_connection.cpp
+++ srcs/connection/handle_connection.cpp
@@ -6,7 +6,7 @@
 /*   By: juvan-de <juvan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/15 13:47:05 by juvan-de      #+#    #+#                 */
-/*   Updated: 2022/03/21 15:35:18 by juvan-de      ########   odam.nl         */
+/*   Updated: 2022/03/21 16:38:53 by juvan-de      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,7 +96,10 @@ void	handle_connection(std::vector<Server> &servers, std::vector<t_client> &clie
 			std::cout << "debug" << std::endl;
 		//	client->request.addto_request(client->fd.fd);
 			if (client->request.isFinished())
+			{
+				client->request.setRequest();
 				client->fd.events = POLLOUT;
+			}
 		}
 		else if (client->fd.revents & POLLOUT)
 		{
