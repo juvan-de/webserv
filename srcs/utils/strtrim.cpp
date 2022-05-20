c63c1fc2b1130ba0ed4d319ddd33b8e5b95b2543
Abel Van bergen en henegouwen <afvanbergen@live.nl>
2022-05-20 10:52:21 +0200

    Removed error.cpp strtrim.cpp and htoi.cpp and removed them from teh header

--- srcs/utils/strtrim.cpp
+++
@@ -1,9 +0,0 @@
-#include <utils.hpp>
-
-std::string	strtrim(std::string toTrim, std::string trimChar)
-{
-	size_t start = toTrim.find_first_not_of(trimChar);
-	size_t end = toTrim.find_last_not_of(trimChar);
-	std::string res(toTrim, start, end - start + 1);
-	return (res);
-}
